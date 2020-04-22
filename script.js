2f354b71271a8aed1271c08a34c3714664bb174b
JCWarmels <jesse.warmels@student.rocva.nl>
2020-04-22 10:29:36 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function changeColor() 
-{
-    document.querySelector('main').style.backgroundColor = "RED";
+function changeColor(pressed) 
+{   
+    switch (pressed) 
+    {
+        case 'saveBackground':
+            color_choice_background = document.querySelector('#Choice').value;
+            break;
+        case 'saveHeader':
+            color_choice_header = document.querySelector('#Choice').value;
+            break;
+        case 'execute':
+            document.querySelector('main').style.backgroundColor = color_choice_background;
+            document.querySelector('h1').style.color = color_choice_header;
+            break;
+    }
 }
